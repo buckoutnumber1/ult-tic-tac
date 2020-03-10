fefc175dad1195e40f2e096a2b28905dfa343a29
buckoutnumber1 <62037944+buckoutnumber1@users.noreply.github.com>
2020-03-10 15:11:24 -0700

    Add files via upload

---
+++ UltimateTicTac.m
@@ -0,0 +1,45 @@
+% Elijah Buckout
+% Peter Holderbein
+% EGR 115
+% Last Edit: 10 March 2020
+
+% "Ultimate Tic-Tac-Toe"
+
+%%
+% Here we set all tic-tac-toe grids to contain empty values.
+
+tic1 = zeros(3);
+tic2 = zeros(3);
+tic3 = zeros(3);
+tic4 = zeros(3);
+tic5 = zeros(3);
+tic6 = zeros(3);
+tic7 = zeros(3);
+tic8 = zeros(3);
+tic9 = zeros(3);
+
+%%
+% Here we welcome the player and display the empty tic-tac-toe grid.
+fprintf('Elijah Buckout & Peter Holderbein present...\n')
+fprintf('//////////////////////////////////////////////////////////////')
+fprintf('//////\n')
+fprintf('***             U L T I M A T E   T I C - T A C - T O E         ')
+fprintf('***\n')
+fprintf('//////////////////////////////////////////////////////////////')
+fprintf('//////\n')
+fprintf('                        _ _ _ | _ _ _ | _ _ _\n')
+fprintf('                        _ _ _ | _ _ _ | _ _ _\n')
+fprintf('                        _ _ _ | _ _ _ | _ _ _\n')
+fprintf('                        ------+-------+-------\n')
+fprintf('                        _ _ _ | _ _ _ | _ _ _\n')
+fprintf('                        _ _ _ | _ _ _ | _ _ _\n')
+fprintf('                        _ _ _ | _ _ _ | _ _ _\n')
+fprintf('                        ------+-------+-------\n')
+fprintf('                        _ _ _ | _ _ _ | _ _ _\n')
+fprintf('                        _ _ _ | _ _ _ | _ _ _\n')
+fprintf('                        _ _ _ | _ _ _ | _ _ _\n')
+fprintf('//////////////////////////////////////////////////////////////')
+fprintf('//////\n')
+
+%%
+%
